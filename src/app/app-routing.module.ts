4617eb1e695b4e4a84ba347b63b76538d2acf5d4
Ananya Kay <2082542@gmail.com>
2019-04-01 14:52:05 +0700

    kay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-<<<<<<< HEAD
-  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
-  { path: 'nid', loadChildren: './nid/nid.module#NidPageModule' }
-=======
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
   { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
   { path: 'protfolio', loadChildren: './protfolio/protfolio.module#ProtfolioPageModule' }
 
->>>>>>> 6fe148c2b973f92bc2933731bef61366e16b64fc
 ];
 @NgModule({
   imports: [
