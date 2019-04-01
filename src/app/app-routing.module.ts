1cb907e4ff374fcd457a0979f5ce3273bfc3b8b1
Ruethairat08 <bbamb.7746@gmai.com>
2019-04-01 05:14:17 +0100

    profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-<<<<<<< HEAD
-  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
-=======
-  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },  { path: 'protfolio', loadChildren: './protfolio/protfolio.module#ProtfolioPageModule' }
->>>>>>> f792b42c76e12260adef7acb828b54a6d3dacf4f
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
+  { path: 'protfolio', loadChildren: './protfolio/protfolio.module#ProtfolioPageModule' }
 
 ];
 @NgModule({
