41b4a2cd3584f03cbd644dd9c4146b2f044b977c
unknown <oraphan.poo@northbkk.ac.th>
2019-04-04 16:27:36 +0700

    up

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview';
 
 @Component({
   selector: 'app-tab2',
@@ -6,6 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  ​
+
+  constructor(private cameraPreview: CameraPreview) { }
+aaaa(){
+  console.log('object')
+  const cameraPreviewOpts: CameraPreviewOptions = {
+    x: 0,
+    y: 0,
+    width: window.screen.width,
+    height: window.screen.height,
+    camera: 'rear',
+    tapPhoto: true,
+    previewDrag: true,
+    toBack: true,
+    alpha: 1
+  };
+  
+  // start camera
+  this.cameraPreview.startCamera(cameraPreviewOpts).then(
+    (res) => {
+      console.log(res)
+    },
+    (err) => {
+      console.log(err)
+    });
+
+    // this.cameraPreview.setOnPictureTakenHandler().subscribe((result) => {
+    //   console.log(result);
+    //   // do something with the result
+    // });
+    
+    
+    // // picture options
+    // const pictureOpts: CameraPreviewPictureOptions = {
+    //   width: 1280,
+    //   height: 1280,
+    //   quality: 85
+    // }
+  
+} ​
 
 }
