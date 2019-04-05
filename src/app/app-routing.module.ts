6e1c0968d04a1359cb9addadc0dd32e6afb6a797
Pacharaphol Kamleachai <Pacharaphol97@hotmail.com>
2019-04-05 10:48:52 +0700

    Score[Boy]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
   { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
-  { path: 'protfolio', loadChildren: './protfolio/protfolio.module#ProtfolioPageModule' }
+  { path: 'protfolio', loadChildren: './protfolio/protfolio.module#ProtfolioPageModule' },  { path: 'score', loadChildren: './score/score.module#ScorePageModule' }
+
 
 ];
 @NgModule({
