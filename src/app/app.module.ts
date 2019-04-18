d620a3cf9ae474df7de3ec44f2529eb847ed69af
Pacharaphol Kamleachai <Pacharaphol97@hotmail.com>
2019-04-18 16:43:45 +0700

    portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ScorePageModule } from './score/score.module';
 import { EvoPageModule } from './evo/evo.module';
 import { ProfileSavePageModule } from './profile-save/profile-save.module';
 import { EditProfilePageModule } from './edit-profile/edit-profile.module';
+import { PortfolioService } from './services/portfolio/portfolio.service';
 
 
 
@@ -28,7 +29,7 @@ import { EditProfilePageModule } from './edit-profile/edit-profile.module';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    StudentService
+    StudentService,PortfolioService
   ],
   bootstrap: [AppComponent]
 })
